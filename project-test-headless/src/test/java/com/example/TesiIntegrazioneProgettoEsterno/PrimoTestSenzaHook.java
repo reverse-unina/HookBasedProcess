edf107afd400799b8d668418dd6e387925f1336c
PorfirioTramontana <PorfirioTramontana@users.noreply.github.com>
2022-02-21 17:25:21 +0100

    Add files via upload

---
+++ project-test-headless/src/test/java/com/example/TesiIntegrazioneProgettoEsterno/PrimoTestSenzaHook.java
@@ -0,0 +1,79 @@
+package com.example.ProvaProcesso;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class PrimoTestSenzaHook {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testPrimoTestSenzaHook() throws Exception {
+    driver.get("http://127.0.0.1:8080/#!/main");
+    driver.findElement(By.xpath("//a[contains(text(),'Users')]")).click();
+    driver.findElement(By.xpath("//a[contains(@href, '#!/main/tasks')]")).click();
+    driver.findElement(By.xpath("//*/text()[normalize-space(.)='✔ Mark as Done']/parent::*")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='✘ Remove'])[1]/following::button[1]")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
